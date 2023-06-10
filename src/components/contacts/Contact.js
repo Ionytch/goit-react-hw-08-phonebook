dd319b529baab5344b8c0530c5d45ec64856af0b
Aleks Ionin <starik.ionytch@gmail.com>
2023-06-11 00:48:16 +0300

    Update Contact.js

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -10,7 +10,7 @@ export const Contact = ({ name }) => {
     const handleDelete = () => dispatch(deleteContact(name.id));
 
     return (<>
-        <p>{name.name}:{name.phone}</p>
+        <p>{name.name}:{name.number}</p>
                                
                 <button type="button" onClick={handleDelete}>DELETE</button>
     </>)
